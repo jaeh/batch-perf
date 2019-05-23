464ee032f00d321c0c333b550dd94e164c375dca
Jascha <jascha@jaeh.at>
2019-05-23 18:49:42 +0200

    add perf tests

--- test/index.mjs
+++ test/index.mjs
@@ -1,4 +1,4 @@
-import { is } from '@magic/test'
+import { is, log } from '@magic/test'
 
 var batch1 = function(list) {
   var out = []
@@ -32,9 +32,41 @@ const lists = [
   [null],
   [[[() => {}]]],
   [[[[() => {}]]]],
+  [[[[() => {}]]],[[[() => {}]]],[[[() => {}]]],[[[() => {}]]],[[[() => {}]]],[[[() => {}]]],[[[() => {}]]],[[[() => {}]]]],
 ]
 
-export default lists.map(t => ({
-  fn: batch1(t),
-  expect: is.deep.equal(batch2(t)),
-}))
+const fns = {
+  batch1,
+  batch2,
+}
+
+const time = arg => {
+  const times = (['batch1', 'batch2']).map((fn, i) => {
+    const startTime = new Date().getTime()
+
+    for (let i = 0; i < 100000; i++) {
+      fns[fn](arg)
+    }
+
+    const endTime = new Date().getTime()
+
+    const timeTaken = endTime - startTime
+    return timeTaken
+  })
+
+  const [time1, time2] = times
+  console.log(`
+batch1 ${log.paint('yellow', time1)}ms - batch2 ${log.paint('yellow', time2)}ms.
+batch1 was ${log.paint('green', parseInt((time1 / time2) * 100))}% faster`)
+
+  return true
+}
+
+export default {
+  unit: lists.map(t => ({
+    fn: batch1(t),
+    expect: is.deep.equal(batch2(t)),
+  })),
+
+  perf: lists.map(t => ({ fn: time(t) })),
+}
